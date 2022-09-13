d012b81fe0ea6c0f8a7cedcac93bc62b160e311a
ACHarrison32 <78120728+ACHarrison32@users.noreply.github.com>
2022-09-13 14:08:51 -0500

    Update Main.cpp

--- Assignments/A1/Main.cpp
+++ Assignments/A1/Main.cpp
@@ -1,35 +1,72 @@
 /*
 Andrew Harrison
-8/23/2022
-Professor Griffin
-10055 Hashmat the Brave Warrior
+Programming Techniques
+8/25/2022
+10226 Hardwood Species
 */
 
-#include <iostream>
+/*=============================================================*/
+/*                     USER-DEFINED FILES                      */
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
 #include <fstream>
-
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+/*=============================================================*/
+/*                        IDENTIFIERS                          */
+#define FileIn(file) freopen(file ".inp", "r", stdin)
+#define FileOut(file) freopen(file ".out", "w", stdout)
+#define FOR(i, a, b) for (int i = a; i <= b; i++)
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define Fill(ar, val) memset(ar, val, sizeof(ar))
+#define PI 3.1415926535897932385
+#define uint64 unsigned long long
+#define int64 long long
+#define all(ar) ar.begin(), ar.end()
+#define pb push_back
+#define bit(n) (1 << (n))
+#define Last(i) (i & -i)
+#define INF 500000000
+#define EPS 1e-7
+#define maxN 10000
+#define newline "\n"
+/*=============================================================*/
+/*                            SCOPE                            */
 using namespace std;
-
-void openFiles(ifstream& read, ofstream& outfile);
-
-int main()
+/*=============================================================*/
+/*                     FUNCTION PROTOTYPES                     */
+void openFiles(ifstream &read, ofstream &display);
+/*=============================================================*/
+/*                       MAIN FUNCTION                         */
+int main() 
 {
-    ifstream read;
-    ofstream outfile;
+  ifstream read;
+  ofstream display;
 
-    openFiles(read, outfile);
+  openFiles(read, display);
 
-    int num1;
-    int num2;
+  //Variable Declarations
+  int num1;
+  int num2;
 
-    while (read >> num1 >> num2)
-    {
-        outfile << abs(num1 - num2);
-        outfile << '\n';
-    }
+  while (read >> num1 >> num2)
+  {
+    display << abs(num1 - num2);
+    display << newline;
+  }
 }
-
-void openFiles(ifstream& infile, ofstream& outfile)
+/*=============================================================*/
+/*                  OPEN FILES FUNCTION                        */
+void openFiles(ifstream& infile, ofstream& display)
 {
     char inFileName[40];
     char outFileName[40];
@@ -38,5 +75,5 @@ void openFiles(ifstream& infile, ofstream& outfile)
     infile.open(inFileName); //open input file
     cout << "Enter Output File Name: ";
     cin >> outFileName;
-    outfile.open(outFileName);//open output file
+    display.open(outFileName);//open output file
 }
