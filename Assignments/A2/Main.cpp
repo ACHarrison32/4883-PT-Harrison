49053f2f13d8b0f70eb545d9762b9de3ff32cefa
ACHarrison32 <78120728+ACHarrison32@users.noreply.github.com>
2022-09-13 14:02:18 -0500

    Update Main.cpp

--- Assignments/A2/Main.cpp
+++ Assignments/A2/Main.cpp
@@ -5,6 +5,8 @@ Programming Techniques
 10226 Hardwood Species
 */
 
+/*=============================================================*/
+/*                     USER-DEFINED FILES                      */
 #include <algorithm>
 #include <bitset>
 #include <cctype>
@@ -19,7 +21,8 @@ Programming Techniques
 #include <stack>
 #include <string>
 #include <vector>
-
+/*=============================================================*/
+/*                        IDENTIFIERS                          */
 #define FileIn(file) freopen(file ".inp", "r", stdin)
 #define FileOut(file) freopen(file ".out", "w", stdout)
 #define FOR(i, a, b) for (int i = a; i <= b; i++)
@@ -35,54 +38,59 @@ Programming Techniques
 #define INF 500000000
 #define EPS 1e-7
 #define maxN 10000
-
+#define newline "\n"
+/*=============================================================*/
+/*                            SCOPE                            */
 using namespace std;
-#define endl "\n"
-
-void openFiles(ifstream& read, ofstream& outfile);
-
-int main()
+/*=============================================================*/
+/*                     FUNCTION PROTOTYPES                     */
+void openFiles(ifstream &read, ofstream &display);
+/*=============================================================*/
+/*                       MAIN FUNCTION                         */
+int main() 
 {
-    ifstream read;
-    ofstream outfile;
+  ifstream read;
+  ofstream display;
 
-    openFiles(read, outfile);
+  openFiles(read, display);
 
-    map<string, int> treeCount;
+  map<string, int> treeCount;
 
-    double num;
-    double totalNum = 0;
-    string tree;
+  // Variable Declarations
+  double num;
+  double totalNum = 0;
+  string tree;
 
-    read >> num;
-    getline(read, tree);
-    read.ignore(256, '\n');
-    while (num > 0)
+  read >> num;
+  getline(read, tree);
+  read.ignore(256, '\n');
+  while (num > 0) 
+  {
+    while (getline(read, tree) && tree != "") 
     {
-        while (getline(read, tree) && tree != "")
-        {
-            treeCount[tree]++;
-            totalNum++;
-        }
-        for (auto it = treeCount.cbegin(); it != treeCount.cend(); it++)
-        {
-            outfile << it->first << " " << fixed << setprecision(4)
-                << it->second / totalNum * 100 << endl;
-        }
-        treeCount.clear();
-        num--;
-        outfile << endl;
+      treeCount[tree]++;
+      totalNum++;
     }
-    return (0);
+    for (auto it = treeCount.cbegin(); it != treeCount.cend(); it++)
+      {
+        display << it->first << " " << fixed << setprecision(4)
+                << it->second / totalNum * 100 << newline;
+      }
+    treeCount.clear();
+    num--;
+    display << newline;
+  }
+  return (0);
 }
-
-void openFiles(ifstream& infile, ofstream& outfile) {
-    char inFileName[40];
-    char outFileName[40];
-    cout << "Enter Input File Name: ";
-    cin >> inFileName;
-    infile.open(inFileName); // open input file
-    cout << "Enter Output File Name: ";
-    cin >> outFileName;
-    outfile.open(outFileName); // open output file
+/*=============================================================*/
+/*                  OPEN FILES FUNCTION                        */
+void openFiles(ifstream &infile, ofstream &display) {
+  char inFileName[40];
+  char outfileName[40];
+  cout << "Enter Input File Name: ";
+  cin >> inFileName;
+  infile.open(inFileName); // open input file
+  cout << "Enter Output File Name: ";
+  cin >> outfileName;
+  display.open(outfileName); // open output file
 }
