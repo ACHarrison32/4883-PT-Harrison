1a9d2101363b4dae628244d8f6cfd7d6883ae10b
ACHarrison32 <78120728+ACHarrison32@users.noreply.github.com>
2022-09-28 15:25:05 -0500

    Create main.cpp

---
+++ Assignments/EZ/EZ_5/main.cpp
@@ -0,0 +1,58 @@
+/*
+Andrew Harrison
+Programming Techniques
+8/25/2022
+10055 HashMat The Brave Warrior
+*/
+
+/*=============================================================*/
+/*                     USER-DEFINED FILES                      */
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+/*=============================================================*/
+/*                        IDENTIFIERS                          */
+#define FileIn(file) freopen(file ".inp", "r", stdin)
+#define FileOut(file) freopen(file ".out", "w", stdout)
+#define FOR(i, a, b) for (int i = a; i <= b; i++)
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define Fill(ar, val) memset(ar, val, sizeof(ar))
+#define PI 3.1415926535897932385
+#define uint64 unsigned long long
+#define int64 long long
+#define all(ar) ar.begin(), ar.end()
+#define pb push_back
+#define bit(n) (1 << (n))
+#define Last(i) (i & -i)
+#define INF 500000000
+#define EPS 1e-7
+#define maxN 10000
+#define newline "\n"
+/*=============================================================*/
+/*                            SCOPE                            */
+using namespace std;
+/*=============================================================*/
+/*                       MAIN FUNCTION                         */
+int main() 
+{
+  //Variable Declarations
+  long long int num1;
+  long long int num2;
+
+  while (cin >> num1 >> num2)
+  {
+    cout << abs(num1 - num2);
+    cout << newline;
+  }
+}
