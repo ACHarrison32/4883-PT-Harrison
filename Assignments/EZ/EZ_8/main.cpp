b5d900ba23b361096ccb2249723994b99e3dce86
ACHarrison32 <78120728+ACHarrison32@users.noreply.github.com>
2022-10-03 00:01:40 -0500

    Create main.cpp

---
+++ Assignments/EZ/EZ_8/main.cpp
@@ -0,0 +1,59 @@
+/*
+Andrew Harrison
+11614 - Etruscan Warriors Never Play Chess
+Programming Techniques
+10/02/2022
+*/
+/*=============================================================*/
+/*                     USER-DEFINED FILES                      */
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+/*=============================================================*/
+/*                        IDENTIFIERS                          */
+#define FileIn(file) freopen(file ".inp", "r", stdin)
+#define FileOut(file) freopen(file ".out", "w", stdout)
+#define FOR(i, a, b) for (int i = a; i <= b; i++)
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define Fill(ar, val) memset(ar, val, sizeof(ar))
+#define PI 3.1415926535897932385
+#define uint64 unsigned long long
+#define int64 long long
+#define all(ar) ar.begin(), ar.end()
+#define pb push_back
+#define bit(n) (1 << (n))
+#define Last(i) (i & -i)
+#define INF 500000000
+#define EPS 1e-7
+#define maxN 10000
+#define newline "\n"
+/*=============================================================*/
+/*                            SCOPE                            */
+using namespace std;
+/*=============================================================*/
+/*                         MAIN FUNCTION                       */
+int main()
+{
+  int testCases;
+  long long int num;
+  long long int ans;
+
+  cin >> testCases;
+  for(int i = 1; i <= testCases; i++)
+    {
+      cin >> num;
+      ans = (sqrt(1 + 8 * num) - 1) / 2;
+      cout << ans << newline;
+    }
+}
